a5668385c33d171ef5b2ee03a5b586e7df370c73
username <zlobalol1@yandex.ru>
2020-05-31 10:21:11 +0300

    init repository

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+import "../scss/main.scss";
+
+// import {
+//     initMainSlider, initProductSlider,
+//     initProductSliderTwo,
+//     initProductSliderThree,
+// } from "./components/sliders";
+
+// import initGlobalScripts from "./global";
+
+// import ProductSlider from '../vue/poduct-slider';
+// new ProductSlider('#product-slider');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // initMainSlider();
+    // initProductSlider();
+    // initProductSliderTwo();
+    // initProductSliderThree();
+    // initGlobalScripts();
+});
+
